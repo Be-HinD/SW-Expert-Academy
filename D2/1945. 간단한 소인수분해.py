a9533121432b6f245c29380d48c230c5d8ba5469
Jeonghyun Kim <kr100475@gmail.com>
2023-05-19 16:45:11 +0900

    재귀

---
+++ D2/1945. 간단한 소인수분해.py
@@ -0,0 +1,25 @@
+def factorization(x):
+    d = 2
+    while d <= x:
+        if x % d == 0:
+            result[d] += 1
+            x = x / d
+        else:
+            d = d + 1
+
+T = int(input())
+
+for tc in range(1, T+1):
+    N = int(input())
+    result = {2:0, 3:0, 5:0, 7:0, 11:0}
+    factorization(N)
+    print('#%d'% tc, end = ' ')
+    for value in result.values():
+        print(value, end = ' ')
+    print()
+
+
+
+"""
+재귀문제
+"""
