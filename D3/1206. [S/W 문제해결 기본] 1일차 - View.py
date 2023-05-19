977e7cac72adbb69ccbad11c74e480116e0df304
Jeonghyun Kim <kr100475@gmail.com>
2023-05-19 20:09:52 +0900

    구현문제

---
+++ D3/1206. [S/W 문제해결 기본] 1일차 - View.py
@@ -0,0 +1,26 @@
+import sys
+sys.stdin = open('1206. [S\input.txt', 'r')
+
+for tc in range(1, 11):
+    num = int(input())
+    height = list(map(int, input().split()))
+    result = 0
+    cnt = 0
+    left = 0
+    right = 0
+    for i in range(len(height)):
+        try:
+            left = max(height[i-2], height[i-1])
+            right = max(height[i+1], height[i+2])
+            sum = max(left, right)
+            if height[i] > sum:
+                result += height[i] - sum
+            else:
+                left = 0
+                right = 0
+                sum = 0
+        except:
+            left = 0
+            right = 0
+            sum = 0
+    print('#{} {}'.format(tc, result))
